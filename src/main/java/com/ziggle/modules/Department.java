795f5be32948c77aa904cb33a6c1b9dc6abf79de
ziggle <muyue1125@gmail.com>
2017-08-17 17:17:16 +0800

    jpa done

---
+++ src/main/java/com/ziggle/modules/Department.java
@@ -0,0 +1,34 @@
+package com.ziggle.modules;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "ziggle_department")
+@Entity
+public class Department {
+
+    private Integer id;
+    private String departmentName;
+
+
+    @GeneratedValue
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
---
+++ src/main/java/com/ziggle/modules/Employee.java
@@ -0,0 +1,75 @@
+package com.ziggle.modules;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "ziggle_empoyee")
+@Entity
+public class Employee {
+    private Integer id;
+    private String lastName;
+    private String email;
+    private Date birth;
+    private Date crateTime;
+    private Department department;
+
+
+    @GeneratedValue
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Temporal(TemporalType.DATE)
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getCrateTime() {
+        return crateTime;
+    }
+
+    public void setCrateTime(Date crateTime) {
+        this.crateTime = crateTime;
+    }
+
+    @JoinColumn(name = "Department_ID")
+    @ManyToOne
+    public Department getDepartment() {
+        return department;
+    }
+
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+}
